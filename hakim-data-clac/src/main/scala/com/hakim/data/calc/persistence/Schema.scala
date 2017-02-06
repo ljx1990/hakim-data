da08e0b3c8744a10defa51ede9709b0b9d9eac94
huangminqiang <hmq@gxb.io>
2017-02-06 23:17:14 +0800

    scala for orm compelete ,slick

--- hakim-data-clac/src/main/scala/com/hakim/data/calc/persistence/Schema.scala
+++ hakim-data-clac/src/main/scala/com/hakim/data/calc/persistence/Schema.scala
@@ -1,30 +1,16 @@
 package com.hakim.data.calc.persistence
+import java.sql.Date
+
 import slick.driver.MySQLDriver.api._
-import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
   * Created by hd on 2017/2/5.
   */
 // Definition of the SUPPLIERS table
-class Suppliers(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
-  def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
-  def name = column[String]("SUP_NAME")
-  def street = column[String]("STREET")
-  def city = column[String]("CITY")
-  def state = column[String]("STATE")
-  def zip = column[String]("ZIP")
+class SpdBackRes(tag: Tag) extends Table[(Date, Int)](tag, "spd_back_res") {
+  def rerDate = column[Date]("rer_date")
+  def isSuccess = column[Int]("is_success")
   // Every table needs a * projection with the same type as the table's type parameter
-  def * = (id, name, street, city, state, zip)
+  def * = (rerDate,isSuccess)
 }
 
-
-// Definition of the SUPPLIERS table
-class Suppliers2(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
-  def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
-  def name = column[String]("SUP_NAME")
-  def street = column[String]("STREET")
-  def city = column[String]("CITY")
-  def state = column[String]("STATE")
-  def zip = column[String]("ZIP")
-  // Every table needs a * projection with the same type as the table's type parameter
-  def * = (id, name, street, city, state, zip)
-}
--- hakim-data-clac/src/main/scala/com/hakim/data/calc/reconciliation/SpdRer.scala
+++ hakim-data-clac/src/main/scala/com/hakim/data/calc/reconciliation/SpdRer.scala
@@ -1,6 +1,15 @@
 package com.hakim.data.calc.reconciliation
 
+import java.sql.Date
+
 import akka.actor._
+import com.hakim.data.calc.persistence.SpdBackRes
+import slick.lifted.TableQuery
+import slick.driver.MySQLDriver.api._
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
 
 
 //回款流水对账行为
@@ -24,7 +33,11 @@ class SpdRer extends Actor with ActorLogging {
   def receive = {
     case BackSectionFlowReconciliation(date) =>
       if (date.trim.nonEmpty) {
-        //
+        val db = Database.forConfig("hakim_data")
+        val spd = TableQuery[SpdBackRes]
+        val queryFuture :Future[Seq[(Date, Int)]]= db.run(spd.result)
+        val res:Seq[(Date, Int)] = Await.result(queryFuture, 5.seconds)
+        print(res)
         sender() ! BackSectionFlowReconciliationResponse(date,true);
       }else{
         sender() ! BackSectionFlowReconciliationResponse(date,false);
